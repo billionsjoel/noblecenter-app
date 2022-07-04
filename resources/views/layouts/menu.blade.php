7b834ffee44de0eac1631cac2b93dbdd6b411577
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-07-04 22:46:51 +0300

    Add donate link

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -37,12 +37,11 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-
-                            @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
@@ -63,6 +62,9 @@
                                 </div>
                             </li>
                         @endguest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('donate') }}">{{ __('Donate') }}</a>
+                        </li>
                     </form>
                 </div>
             </div>
