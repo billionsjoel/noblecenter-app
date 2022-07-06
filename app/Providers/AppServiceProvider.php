f2a094e2c17cf66e9632f51ac6c6ab78991a1e56
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-07-06 05:50:56 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
          }
+$this -> app -> bind('path.public', function()
+    {
+    return base_path('public_html');
+    });
         //
     }
 
