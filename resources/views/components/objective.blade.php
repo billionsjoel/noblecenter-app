8869b96f71255074b0fa0ea7588f41b34138934d
Atugonza ( Billions ) Joel <joelatugonza@gmail.com>
2022-07-20 10:34:06 +0300

    Update objective.blade.php

--- resources/views/components/objective.blade.php
+++ resources/views/components/objective.blade.php
@@ -3,19 +3,30 @@
         <h3 class="text-blue">more ...</h3>
         <div class="cards">
             <div class="content left">
-                <ul>
-                    <li>Fighting corruption</li>
-                    <li>Technology dissemination and promotions</li>
-                    <li>Empowerment of vulnerable groups (Elderly; Disabled; Orphans; Street children; Internally
-                        Displaced
-                        People; Widows; Albinos; Persons with developmental and handicap disabilities, etc.)</li>
-                    <li>Prevention against Child Trafficking; Human exploitations; torture and violence; etc.</li>
-                    <li>Tourism and Wildlife</li>
-                    <li>Climate change Programs</li>
-                    <li>Human Rights Activities e.g Children’s rights, Women’s rights,etc.</li>
-                    <li>Science discoveries, Innovations and Emerging Technologies (science is not complete until it is
-                        communicated)</li>
-                </ul>
+                <h5 class="text-blue">WHAT IS SOCIAL AND BEHAVIOR CHANGE?
+                </h5>
+                <p>Social and Behavior Change (SBC) is the process of applying research and theory - based
+                    approaches to
+                    identify opportunities to change behaviors at the individual, community, and/or societal levels.
+                </p>
+
+                <h6 class="text-blue">WHY SOCIAL AND BEHAVIOR CHANGE?</h6>
+
+                <p> Human behavior is complex - a combination of influences is often necessary to trigger a person
+                    to
+                    test, adopt and sustain new behaviors.
+
+                    The vast majority of development interventions have one thing in common: their goals can be
+                    achieved
+                    only if the target groups start practicing new behaviors.
+
+                    Many development interventions fail because they are based on incorrect assumptions why people
+                    do
+                    not practice the promoted behaviors.
+
+                    Understanding exactly which factors prevent people from practicing the promoted behaviors and
+                    subsequently addressing the identified barriers is the single most effective way to achieve the
+                    desired behavior.</p>
             </div>
             <div class="content card right">
                 <img src="{{ asset('images/clean-water.jpeg') }}"
